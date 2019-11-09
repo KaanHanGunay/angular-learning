3c1b7ce8f93da382b68d68f8808512d8a5d4b8e5
kaangunay16 <kaangunay16@gmail.com>
2019-11-09 19:20:13 +0300

    Starting new project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
 import {FormsModule} from '@angular/forms';
-import { Homework3Component } from './homework3/homework3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent,
-    Homework3Component
   ],
   imports: [
     BrowserModule,

4079a122d7eec9491043128ac9c149bb989089d7
Kaan Han Günay <kaangunay16@gmail.com>
2019-11-19 09:19:20 +0300

    Component communication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  serverElements = [{type: 'server', name: 'testServer', content: 'testContent'}];
+
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
 }
